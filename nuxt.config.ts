149a21c22528625485145b7c192b8608ea733b9a
shaneski <shane@micronetclub.co.jp>
2023-01-27 15:38:22 +0900

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
             ],
         }
     },
-    //modules: ["formidable"],
+    modules: ["formidable"],
     runtimeConfig: {
         openaiApiKey: '',
         public: {
